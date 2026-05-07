2d2eddc48350b1e493e607093220491ce54b74d9
shinoharakymsk-png <shinohara.kymsk@gmail.com>
2026-05-07 18:37:41 +0900

    Fix server.js formatting and logging message

--- server.js
+++ server.js
@@ -49,4 +49,4 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => console.log(`Server running` bits));
+http.listen(PORT, () => console.log(`Server running on port ${PORT}`));
